762031402cc3e121fe4e16a6a19e851bad8e5ac1
Ethan <greymesa71@gmail.com>
2022-07-02 14:07:56 +0200

    Fixed alignment issue

--- source/main.d
+++ source/main.d
@@ -11,14 +11,14 @@ import common.scheduler        : initScheduler;
 version (X86_64) import arch.amd64;
 version (X86_64) import arch.amd64.drivers.legacy.keyboard;
 
-__gshared FrameBufferRequest frameBufferRequest       = FrameBufferRequest(FrameBufferRequestID, 0);
-__gshared BootloaderInfoRequest bootloaderInfoRequest = BootloaderInfoRequest(BootloaderInfoID, 0);
-__gshared MemoryMapRequest memoryMapRequest           = MemoryMapRequest(MemoryMapID, 0);
-__gshared XSDTPointerRequest xsdtPointerRequest       = XSDTPointerRequest(XSDTPointerID, 0);
-__gshared StackSizeRequest stackSizeRequest           = StackSizeRequest(StackSizeID, 0, null, 32768);
-__gshared HigherHalfRequest higherHalfRequest         = HigherHalfRequest(HigherHalfID, 0);
-__gshared KernelAddressRequest kernelAddressRequest   = KernelAddressRequest(KernelAddressID, 0);
-__gshared ModuleRequest moduleRequest                 = ModuleRequest(ModuleID, 0);
+align(8) __gshared FrameBufferRequest frameBufferRequest       = FrameBufferRequest(FrameBufferRequestID, 0);
+align(8) __gshared BootloaderInfoRequest bootloaderInfoRequest = BootloaderInfoRequest(BootloaderInfoID, 0);
+align(8) __gshared MemoryMapRequest memoryMapRequest           = MemoryMapRequest(MemoryMapID, 0);
+align(8) __gshared XSDTPointerRequest xsdtPointerRequest       = XSDTPointerRequest(XSDTPointerID, 0);
+align(8) __gshared StackSizeRequest stackSizeRequest           = StackSizeRequest(StackSizeID, 0, null, 32768);
+align(8) __gshared HigherHalfRequest higherHalfRequest         = HigherHalfRequest(HigherHalfID, 0);
+align(8) __gshared KernelAddressRequest kernelAddressRequest   = KernelAddressRequest(KernelAddressID, 0);
+align(8) __gshared ModuleRequest moduleRequest                 = ModuleRequest(ModuleID, 0);
 
 
 extern (C) void main() {	
@@ -35,8 +35,6 @@ extern (C) void main() {
 	initAlloc();
 
 	initArch(memoryMapRequest.response, xsdtPointerRequest.response, kernelAddressRequest.response);
-
-	//shellMain();
 
 	initScheduler(moduleRequest.response);
 
